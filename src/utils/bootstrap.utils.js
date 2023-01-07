079ea18abdd4b3673faae9d5fd40979c0210bcf8
Andre Silva <andre.silva@mercedes-benz.io>
2023-01-07 15:32:37 +0000

    [data-structure-playground]: refactoring the code (creation of services and utils)

--- src/utils/bootstrap.utils.js
+++ src/utils/bootstrap.utils.js
@@ -1,3 +1,5 @@
+import { ArrayService } from "../modules/linear-data-structures/services/array.services";
+
 export class Bootstrap {
     constructor() { }
 
@@ -20,110 +22,6 @@ export class Bootstrap {
     }
 
     createDOMEvents(element) {
-        const arrayInputEl = document.getElementById('arrayInput');
-
-        const showArrayButton = document.getElementById('showArrayButton');
-        showArrayButton.addEventListener('click', () => {
-            this.promptMessage('See array', element.join(' - '));
-        });
-
-        const sortArrayButton = document.getElementById('sortArrayButton');
-        sortArrayButton.addEventListener('click', () => {
-            element.sort();
-            this.promptMessage('Sort array', element.join(' - '));
-        });
-
-        const pushArrayButton = document.getElementById('pushArrayButton');
-        pushArrayButton.addEventListener('click', () => {
-            element.push(arrayInputEl.value);
-
-            this.promptMessage('Push element into array', element.join(' - '));
-        });
-
-        const findArrayButton = document.getElementById('findArrayButton');
-        findArrayButton.addEventListener('click', () => {
-            this.promptMessage('Find element into array', element.find(arrayInputEl.value));
-        });
-
-        const unshiftArrayButton = document.getElementById('unshiftArrayButton');
-        unshiftArrayButton.addEventListener('click', () => {
-            element.unshift(arrayInputEl.value);
-
-            this.promptMessage('Unshift element into array', element.join(' - '));
-        });
-
-        const lengthArrayButton = document.getElementById('lengthArrayButton');
-        lengthArrayButton.addEventListener('click', () => {
-            this.promptMessage('See array length', element.length());
-        });
-
-        const getArrayButton = document.getElementById('getArrayButton');
-        getArrayButton.addEventListener('click', () => {
-            this.promptMessage('Get an element from array', element.get(arrayInputEl.value));
-        });
-
-        const updateArrayButton = document.getElementById('updateArrayButton');
-        updateArrayButton.addEventListener('click', () => {
-            const updateArrayInput = document.getElementById('updateArrayInput');
-            const updateArrayIndexInput = document.getElementById('updateArrayIndexInput');
-            element.update(updateArrayIndexInput.value, updateArrayInput.value);
-
-            this.promptMessage('Update an array element', element.join(' - '));
-        });
-
-        const sliceArrayButton = document.getElementById('sliceArrayButton');
-        sliceArrayButton.addEventListener('click', () => {
-            const sliceStartArrayInput = document.getElementById('sliceStartArrayInput');
-            const sliceEndArrayInput = document.getElementById('sliceEndArrayInput');
-            const newElement = element.slice(sliceStartArrayInput.value, sliceEndArrayInput.value);
-
-            this.promptMessage('Slice', JSON.stringify(newElement));
-        });
-
-        const spliceArrayButton = document.getElementById('spliceArrayButton');
-        spliceArrayButton.addEventListener('click', () => {
-            const spliceStartArrayInput = document.getElementById('spliceStartArrayInput');
-            const spliceQuantityArrayInput = document.getElementById('spliceQuantityArrayInput');
-            const spliceArrayInput = document.getElementById('spliceArrayInput');
-            const newElement = element.splice(spliceStartArrayInput.value, spliceQuantityArrayInput.value, spliceArrayInput.value);
-
-            this.promptMessage('Splice', JSON.stringify(newElement));
-        });
-
-        const popArrayButton = document.getElementById('popArrayButton');
-        popArrayButton.addEventListener('click', () => {
-            element.pop();
-            
-            this.promptMessage('Pop element from array', element.join(' - '));
-        });
-
-        const shiftArrayButton = document.getElementById('shiftArrayButton');
-        shiftArrayButton.addEventListener('click', () => {
-            element.shift();
-            
-            this.promptMessage('Shift element from array', element.join(' - '));
-        });
-    }
-
-    promptMessage(message, element) {
-        const promptContainerEl = document.getElementsByClassName('array__terminal-prompt')[0];
-        const promptEl = document.getElementsByClassName('array__terminal-prompt--old-commands')[0];
-        const currentLineEl = document.getElementsByClassName('terminal-prompt__current-line')[0];
-        currentLineEl.innerHTML = message;
-
-        setTimeout(() => {
-            const instruction = document.createElement('span');
-            instruction.innerHTML = `> ${message}`;
-            promptEl.append(instruction);
-
-            const alert = document.createElement('a');
-            alert.setAttribute('href', '#');
-            alert.innerHTML = element;
-            promptEl.append(alert);
-
-            currentLineEl.innerHTML = '';
-
-            promptContainerEl.scrollTop = promptContainerEl.scrollHeight;
-        }, 500);
+        ArrayService.prototype.createArrayEvents(element);
     }
 }
