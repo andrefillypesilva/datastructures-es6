af494199f79e9db129e9ed21ae39490b4d1e96a6
Andre Silva <andre.silva@mercedes-benz.io>
2023-02-20 23:32:14 +0000

    [data-structures-playground]: implementation of queue and stack components

--- src/modules/linear-data-structures/components/queue/queue.component.js
+++ src/modules/linear-data-structures/components/queue/queue.component.js
@@ -1,3 +1,27 @@
-export class Queue {
-    constructor() {}
+import { LinkedList } from '../linked-list/linked-list.component';
+
+export class Queue extends LinkedList {
+    constructor() {
+        super();
+    }
+
+    // [time complexity]: O(n)
+    lookup(value) {
+        this.lookup(value);
+    }
+
+    // [time complexity]: O(1)
+    enqueue(value) {
+        this.append(value);
+    }
+
+    // [time complexity]: O(1)
+    dequeue() {
+        this.delete(0);
+    }
+
+    // [time complexity]: O(1)
+    peek() {
+        return this.head;
+    }
 }
--- src/modules/linear-data-structures/components/stack/stack.component.js
+++ src/modules/linear-data-structures/components/stack/stack.component.js
@@ -1,3 +1,27 @@
-export class Stack {
-    constructor() {}
+import { LinkedList } from "../linked-list/linked-list.component";
+
+export class Stack extends LinkedList {
+    constructor() {
+        super();
+    }
+
+    // [time complexity]: O(n)
+    lookup(value) {
+        this.lookup(value);
+    }
+
+    // [time complexity]: O(1)
+    pop() {
+        this.delete(0);
+    }
+
+    // [time complexity]: O(1)
+    push(value) {
+        this.prepend(value);
+    }
+
+    // [time complexity]: O(1)
+    peek() {
+        return this.head;
+    }
 }
