b2466ee22db58d64a634f3fdae420939c7f9e568
Andre Silva <andre.silva@mercedes-benz.io>
2023-03-07 22:09:23 +0000

    [data-structures-playground]: templating binary-tree

--- src/modules/non-linear-data-structures/services/binary-tree.service.js
+++ src/modules/non-linear-data-structures/services/binary-tree.service.js
@@ -25,11 +25,11 @@ export class BinaryTreeService {
         li.append(span);
 
         const children = this.createTreeBranches(this.binaryTree.root);
-        const childrenList = document.createElement('ul');
-        childrenList.append(children[0]);
-        childrenList.append(children[1]);
+        const ulChildren = document.createElement('ul');
+        ulChildren.append(children[0]);
+        ulChildren.append(children[1]);
 
-        li.append(childrenList);
+        li.append(ulChildren);
 
         ul.append(li);
         tree.append(ul);
@@ -39,21 +39,77 @@ export class BinaryTreeService {
 
     createTreeBranches(root) {
         const liChildLeft = document.createElement('li');
-        const spanChildLeft = document.createElement('span');
-        spanChildLeft.className = 'tf-nc';
-        spanChildLeft.innerHTML = root.left ? root.left.key : '';
+        if (root.left) {
+            const spanChildLeft = document.createElement('span');
+            spanChildLeft.className = 'tf-nc';
+            spanChildLeft.innerHTML = root.left.key;
+            liChildLeft.append(spanChildLeft);
+
+            const ul = document.createElement('ul');
+            const children = this.createTreeBranches(root.left);
+
+            if (!(children[0].className === 'binary-tree__empty-node' &&
+                children[1].className === 'binary-tree__empty-node')) {
+                ul.append(children[0]);
+                ul.append(children[1]);
+                liChildLeft.append(ul);
+            }
+        } else {
+            liChildLeft.className = 'binary-tree__empty-node';
+
+            const spanChildLeft = document.createElement('span');
+            spanChildLeft.className = 'tf-nc';
+            spanChildLeft.innerHTML = 'null';
+            liChildLeft.append(spanChildLeft);
+        }
 
         const liChildRight = document.createElement('li');
-        const spanChildRight = document.createElement('span');
-        spanChildRight.className = 'tf-nc';
-        spanChildRight.innerHTML = root.right ? root.right.key : '';
+        if (root.right) {
+            const spanChildRight = document.createElement('span');
+            spanChildRight.className = 'tf-nc';
+            spanChildRight.innerHTML = root.right.key;
+            liChildRight.append(spanChildRight);
 
-        liChildLeft.append(spanChildLeft);
-        liChildRight.append(spanChildRight);
+            const ul = document.createElement('ul');
+            const children = this.createTreeBranches(root.right);
+
+            if (!(children[0].className === 'binary-tree__empty-node' &&
+                children[1].className === 'binary-tree__empty-node')) {
+                ul.append(children[0]);
+                ul.append(children[1]);
+                liChildRight.append(ul);
+            }
+
+        } else {
+            liChildRight.className = 'binary-tree__empty-node';
+
+            const spanChildRight = document.createElement('span');
+            spanChildRight.className = 'tf-nc';
+            spanChildRight.innerHTML = 'null';
+            liChildRight.append(spanChildRight);
+        }
 
         return [
             liChildLeft,
             liChildRight,
         ];
+
+        // const liChildLeft = document.createElement('li');
+        // const spanChildLeft = document.createElement('span');
+        // spanChildLeft.className = 'tf-nc';
+        // spanChildLeft.innerHTML = root.left ? root.left.key : '';
+
+        // const liChildRight = document.createElement('li');
+        // const spanChildRight = document.createElement('span');
+        // spanChildRight.className = 'tf-nc';
+        // spanChildRight.innerHTML = root.right ? root.right.key : '';
+
+        // liChildLeft.append(spanChildLeft);
+        // liChildRight.append(spanChildRight);
+
+        // return [
+        //     liChildLeft,
+        //     liChildRight,
+        // ];
     }
 }
