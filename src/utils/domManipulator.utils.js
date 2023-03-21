88ef5080bca476c476ee03f9e3dbf1f5b86baabe
Andre Silva <andre.silva@mercedes-benz.io>
2023-03-21 18:51:47 +0000

    [data-structures-playground]: removing files that are not important for the package

--- src/utils/domManipulator.utils.js
+++
@@ -1,8 +0,0 @@
-export class DOMManipulator {
-    constructor () { }
-
-    getDOMElement(elementId, callback) {
-        const element = document.getElementById(elementId);
-        element.addEventListener('click', callback);
-    }
-}
