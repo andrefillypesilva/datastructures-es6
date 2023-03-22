89d5f878c7e6aa72f1b7fb572d22ddd38ee70b9d
Andre Silva <andre.silva@mercedes-benz.io>
2023-03-22 00:09:40 +0000

    [datastructures-es6]: fixing importing of files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { Array, LinkedList, Queue, Stack } from './modules/linear-data-structures/linear-data-structures.module';
-import { BinaryTree } from './modules/non-linear-data-structures/non-linear-data-structures.module';
+import { Array, LinkedList, Queue, Stack } from './modules/linear-data-structures/linear-data-structures.module.js';
+import { BinaryTree } from './modules/non-linear-data-structures/non-linear-data-structures.module.js';
 
 export {
     Array,
