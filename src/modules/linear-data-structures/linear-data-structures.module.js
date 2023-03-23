6ab484dcf7a048835ea86e1d44872567d2340dae
Andre Silva <andre.silva@mercedes-benz.io>
2023-03-23 23:47:15 +0000

    [datastructures-es6]: documenting modules

--- src/modules/linear-data-structures/linear-data-structures.module.js
+++ src/modules/linear-data-structures/linear-data-structures.module.js
@@ -3,6 +3,11 @@ import { LinkedList } from "./components/linked-list/linked-list.component.js";
 import { Queue } from "./components/queue/queue.component.js";
 import { Stack } from "./components/stack/stack.component.js";
 
+/**
+ * @author André Fillype Silva <andrefillype10@gmail.com>
+ * @description Module to export all linear data structures in the project.
+ * @module linear-data-structures
+ */
 export {
     Array,
     LinkedList,
--- src/modules/non-linear-data-structures/non-linear-data-structures.module.js
+++ src/modules/non-linear-data-structures/non-linear-data-structures.module.js
@@ -1,3 +1,8 @@
 import { BinaryTree } from "./components/binary-tree/binary-tree.component.js";
 
+/**
+ * @author André Fillype Silva <andrefillype10@gmail.com>
+ * @description Module to export all non linear data structures in the project.
+ * @module non-linear-data-structures
+ */
 export { BinaryTree };
